566ad9eb7b1713abfde7f40983916662e75fae7e
Jarmo Puttonen <git@rot.fi>
2019-05-24 13:00:56 +0300

    Try to automate DNS-server for Lets Encrypt

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ MYPUBLICIP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4 -s)
 apt update -y && apt install -y default-jre python-pip && pip install dnslib
 mkdir -p /usr/local/collaborator/
 cp *.jar /usr/local/collaborator/burp.jar
+cp dnshook.sh /usr/local/collaborator/
 
 cp collaborator.config /usr/local/collaborator/collaborator.config
 sed -i "s/INT_IP/$MYPRIVATEIP/g" /usr/local/collaborator/collaborator.config
@@ -27,18 +28,12 @@ echo "options edns0" >> /etc/resolv.conf
 echo "search eu-north-1.compute.internal" >> /etc/resolv.conf
 
 echo ""
-echo ""
-echo "IMPORTANT: When certbot shows you the DNS TXT-record, don't press enter."
-echo "Run the below cmd and give it the value from certbot: "
-echo "sudo python /usr/local/lib/python2.7/dist-packages/dnslib/fixedresolver.py -r '_acme-challenge.$DOMAIN. 10 IN TXT \"TXT_VALUE_HERE\"'"
-echo "COPY THE ABOVE COMMAND BEFORE CONTINUING!"
-echo ""
 echo "CTRL-C if you don't need to obtain certificates."
 echo ""
 read -p "Press enter to continue"
 
 rm -rf /usr/local/collaborator/keys
-./certbot-auto certonly -d $DOMAIN -d *.$DOMAIN  --server https://acme-v02.api.letsencrypt.org/directory --manual --agree-tos --no-eff-email --manual-public-ip-logging-ok --preferred-challenges dns-01
+./certbot-auto certonly --manual-auth-hook dnshook.sh -d $DOMAIN -d *.$DOMAIN  --server https://acme-v02.api.letsencrypt.org/directory --manual --agree-tos --no-eff-email --manual-public-ip-logging-ok --preferred-challenges dns-01
 
 CERT_PATH=/etc/letsencrypt/live/$DOMAIN/
 mkdir -p /usr/local/collaborator/keys/
