cc9a4ddda2ccc2dd2a0c7151b62b6bf0541be8d2
fruh <fruh>
2019-08-01 15:53:48 +0200

    new functionality which set up web server at port https://domain:1337 with:
    - basic auth
    - here are served burp collaborator logs
    - it enables fast review of burp collaborator interaction without burp client

--- startcollab.sh
+++ startcollab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xms10m -Xmx200m -XX:GCTimeRatio=19 -jar /usr/local/collaborator/burp*.jar --collaborator-server --collaborator-config=/usr/local/collaborator/collaborator.config
+java -Xms10m -Xmx200m -XX:GCTimeRatio=19 -jar /usr/local/collaborator/burp*.jar --collaborator-server --collaborator-config=/usr/local/collaborator/collaborator.config | grep --line-buffered -vE ' : Received HTTP[S]? polling request from \[' >> /var/www/collaborator/burp.txt
