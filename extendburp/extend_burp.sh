aea4f1b1e95f3e7837ee485deb37ba26ee94edb9
Jarmo Puttonen <github@rot.fi>
2019-10-21 10:55:36 +0300

    Use cmdline params in extend_burp.sh

--- extendburp/extend_burp.sh
+++ extendburp/extend_burp.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # This script uses ugly DNS-hack which allows you to redirect all main domain (non-subdomain) HTTP-requests to another IP.
-# For example:
-# gcwtg9e6xjny3wvglltchxnjjap8dx.collab.fi -> Passed to Burp Collaborator
-# www.collab.fi -> Passed to 1.2.3.4
-# collab.fi -> Passed to 1.2.3.4
+# For example, running with "./extend_burp.sh 1.2.3.4 collab.fi" configures the following setup:
+# gcwtg9e6xjny3wvglltchxnjjap8dx.collab.fi -> Passed to Burp Collaborator.
+# 828jyjcah820y7zp5rl4u784avgp4e.collab.fi -> Passed to Burp Collaborator.
+# www.collab.fi -> Passed to 1.2.3.4.
+# collab.fi -> Passed to 1.2.3.4.
 
-# Replace with the web server IP that will serve HTML for the main domain.
-TARGET_IP=1.2.3.4
-# Replace with your collaborator domain.
-MAIN_DOMAIN=collab.fi
+# Web server IP that will serve HTML for the main domain.
+TARGET_IP=$1
+# Your collaborator domain.
+MAIN_DOMAIN=$2
 
 apt install -y python-nfqueue build-essential python-dev libnetfilter-queue-dev
 pip install scapy
