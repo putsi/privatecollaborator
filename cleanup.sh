56c0ca40dfdf4a47caccd662b645f2490c6ec532
putsi <github@rot.fi>
2022-02-15 13:26:04 +0200

    Fix concurrency issues with certbot DNS hooks

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-pkill -f fixedresolver.py
+# Kill fixed resolver.
+pkill -f dnslib.fixedresolver
+
+# Restart Burp if it was started before renewal process.
+if [[ "$CERTBOT_AUTH_OUTPUT" == "active" ]]
+then
+    service burpcollaborator start
+fi
+
+rm -f /tmp/collab.zonefile
+
--- dnshook.sh
+++ dnshook.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-python3 /usr/local/lib/python$(python3 -V | cut -d' ' -f 2 | cut -d. -f 1,2)/dist-packages/dnslib/fixedresolver.py -r "_acme-challenge.$CERTBOT_DOMAIN. 10 IN TXT \"$CERTBOT_VALIDATION\"" -a $1 &>/dev/null &disown
-sleep 5
+# Zonefile must be used because certbot tries to start multiple DNS-resolvers for wildcards.
+echo "_acme-challenge.$CERTBOT_DOMAIN. 10 IN TXT \"$CERTBOT_VALIDATION\"" >> /tmp/collab.zonefile
+pkill -f dnslib.fixedresolver &>/dev/null
+
+# Echo burp service status for CERTBOT_AUTH_OUTPUT so that cleanup can start Burp service if it was started before.
+echo "$(systemctl show -p ActiveState --value burpcollaborator)"
+service burpcollaborator stop &>/dev/null
+
+python3 -m dnslib.fixedresolver -a $1 --zonefile /tmp/collab.zonefile &>/dev/null &disown
+
+# Sleep to avoid DNS propagation issues.
+sleep 10
+
